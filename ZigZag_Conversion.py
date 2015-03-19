eb027a4963ae8088251a4ef0e7c55e0076256710
Mangosteen <user@elong-test-E.local>
2015-03-19 13:28:53 +0800

    zigzag right

--- ZigZag_Conversion.py
+++ ZigZag_Conversion.py
@@ -1,24 +1,37 @@
+#coding:utf-8
+'''
+注意事项：考虑nRows=1等临界情况
+考虑i 和 x各自循环次数，可以富余不可以不够
+简单合并排序是不可靠的，因为(1, 11)和(11, 1)合并后均为111
+'''
 def convert(self, s, nRows):
-	position = []
+	positionx = []
+	positiony = []
 	result = ''
 	if nRows == 1:
 		return s
-	lineNum = (len(s)/(2*nRows-2))+2
-	print lineNum
-	for i in xrange(0,2*lineNum-1, nRows-1):
+	lineNum = (len(s)/(2*nRows-2))+1
+	for i in xrange(0,lineNum*(nRows-1), nRows-1):
 		for x in xrange(0,2*nRows-2):
 			if(x<nRows):
-				position.append('%d%d'%(x, i))
+				positionx.append('%d' % (x))
+				positiony.append('%d' % (i))
 			else:
 				i+=1
 				x=2*nRows-2-x
-				position.append('%d%d'%(x, i))
+				positionx.append('%d' % (x))
+				positiony.append('%d' % (i))
 	sToList = list(s)
-	tmp = zip(position, s)
-	tmp.sort()
+	for each in xrange(0,len(positionx)):
+		positionx[each] = int(positionx[each])
+	for each in xrange(0,len(positiony)):
+		positiony[each] = int(positiony[each])
+	tmp = zip(positionx, positiony, s)
+	# tmp.sort(key=itemgetter(0,1))
+	tmp = sorted(tmp, key=lambda student : student[0])
 	for each in tmp:
-		result+=each[1]
+		result+=each[2]
 	return result
 
 
-print convert(0, 'PAYPALISHIRING', 6)
+print convert(0, 'Apalindromeisaword,phrase,number,orothersequenceofunitsthatcanbereadthesamewayineitherdirection,withgeneralallowancesforadjustmentstopunctuationandworddividers.', 3)
